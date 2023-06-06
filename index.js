04fcfd7a11c12a42cd3ef506fa521d46c92ef7ce
Anna Harder <annaqharder@gmail.com>
2023-06-06 13:51:40 -0600

    added nodemon

--- index.js
+++ index.js
@@ -12,11 +12,24 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback",
     },
-    (accessToken) => {
-      console.log(accessToken);
+    (accessToken, refreshToken, profile, done) => {
+      console.log("access token", accessToken);
+      console.log("refresh token", refreshToken);
+      console.log("profile", profile);
     }
   )
 );
+
+// route handler to Google OAuth
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+// route handler for google/auth/callback
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT);
