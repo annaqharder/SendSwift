91eb2c0a45283edde9e3f2279ccd91d8a63a01ed
Anna Harder <annaqharder@gmail.com>
2023-06-07 11:00:01 -0600

    passport callbacks, encoding users, deserializing user, enabling cookies

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require("./models/user"); // needs to be before line 4
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+require("./models/User"); // needs to be before line 4
 require("./services/passport");
 const authRoutes = require("./routes/authRoutes");
-const keys = require("./config/keys");
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+  cookieSession({
+    // 30 days -> max time the cookie will last
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 authRoutes(app);
 
 const PORT = process.env.PORT || 5001;
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,7 +8,13 @@ module.exports = (app) => {
       scope: ["profile", "email"],
     })
   );
-
   // route handler for google/auth/callback
   app.get("/auth/google/callback", passport.authenticate("google"));
+
+  // route to log out user
+
+  //
+  app.get("/api/current_user", (req, res) => {
+    res.send(req.user);
+  });
 };
