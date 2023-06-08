23fc7df0b904906dd577ec33a78401addb6c1fd6
Anna Harder <annaqharder@gmail.com>
2023-06-08 11:01:57 -0600

    added test.js for async & await syntax

---
+++ client/test.js
@@ -0,0 +1,17 @@
+// make an ajax request
+// use "fetch" function
+
+// function fetchAlbums() {
+//   fetch("https://rallycoding.herokuapp.com/api/music_albums").then((res) =>
+//     res.json().then((json) => console.log(json))
+//   );
+// };
+
+async function fetchAlbums() {
+  const res = await fetch("https://rallycoding.herokuapp.com/api/music_albums");
+  const json = await res.json();
+
+  console.log(json);
+}
+
+fetchAlbums();
