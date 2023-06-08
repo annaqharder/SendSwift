694763b0c7f349d17de4624ee780f96e71317507
Anna Harder <annaqharder@gmail.com>
2023-06-08 11:12:04 -0600

    refactor with async/await

--- client/test.js
+++ client/test.js
@@ -7,11 +7,11 @@
 //   );
 // };
 
-async function fetchAlbums() {
+const fetchAlbums = async () => {
   const res = await fetch("https://rallycoding.herokuapp.com/api/music_albums");
   const json = await res.json();
 
   console.log(json);
-}
+};
 
 fetchAlbums();
