49cccbaf4fa5678cd85574c0fa18d0ac012049aa
Anna Harder <annaqharder@gmail.com>
2023-06-09 13:33:48 -0600

    added action folder + index.js + types.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,18 @@
 import react, { Component } from "react";
 class Header extends Component {
   render() {
-    return <div>Header</div>;
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a className="left brand-logo">SendSwift</a>
+          <ul className="right">
+            <li>
+              <a>Login With Google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
 
