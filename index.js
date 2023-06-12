85fc76d75c5d189db4d46b7f13f8aac70bc85f79
Anna Harder <annaqharder@gmail.com>
2023-06-12 16:15:30 -0600

    creating a charge with stripe

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ const keys = require("./config/keys");
 require("./models/User"); // needs to be before line 4
 require("./services/passport");
 const authRoutes = require("./routes/authRoutes");
+const billingRoutes = require("./routes/billingRoutes");
+const bodyParser = require("body-parser");
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     // 30 days -> max time the cookie will last
@@ -23,6 +26,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 authRoutes(app);
+billingRoutes(app);
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT);
