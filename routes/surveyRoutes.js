0c3b3256cbe3371ee192ce4b5370e6fcb71f54a5
Anna Harder <annaqharder@gmail.com>
2023-06-13 14:56:16 -0600

    created surveyRoute

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,23 @@
+const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
 
+const Survey = mongoose.model("surveys");
+
 module.exports = (app) => {
-  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {});
+  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+
+    // create new instance of Survey
+    const survey = new Survey({
+      title: title,
+      subject: subject,
+      body: body,
+      recipients: recipients.split(",").map((email) => {
+        return { email: email };
+      }),
+      _user: req.user.id,
+      dateSent: Date.now(),
+    });
+  });
 };
